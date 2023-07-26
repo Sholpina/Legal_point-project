f70bfcce8d925b784db02418d8a3885053e27312
Sholpina <30983083+Sholpina@users.noreply.github.com>
2023-07-26 22:04:14 +0400

    updated

--- blog/models.py
+++ blog/models.py
@@ -3,8 +3,9 @@ from django.db import models
 
 class Blog(models.Model):
         title = models.CharField(max_length=200)
-        description = models.TextField()
+        image = models.ImageField(upload_to='media/blog/images/')
         date = models.DateField()
+        description = models.TextField()
         tag = models.CharField(max_length=50)
 
         def __str__(self):
