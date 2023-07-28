cdadf1f7fd44a381af62c0ad59a5b94b3bb21bb9
Sholpina <30983083+Sholpina@users.noreply.github.com>
2023-07-29 01:31:17 +0400

    update

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,5 @@
+from datetime import timezone
+
 from django.db import models
 
 
@@ -5,11 +7,16 @@ class Blog(models.Model):
         title = models.CharField(max_length=100)
         description = models.TextField()
         date = models.DateField()
-        image = models.ImageField(upload_to='media/blog/images', null=True, blank=True)
+        image = models.ImageField(upload_to='media/blog/images', null=True)
         tag = models.CharField(max_length=50, null=True, blank=True)
 
         def __str__(self):
                 return self.title
+
+        # def was_published_recently(self, date):
+        #         now = timezone.now()
+        #         time_since_publication = now - self.date
+        #         return time_since_publication
 
 # class Tag(models.Model):
 #     article = models.ForeignKey(
